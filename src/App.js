524bbb7863a7afc67736507285ade136ef3afc8b
Emre <emrekurtt1655@gmail.com>
2021-01-03 20:05:41 +0100

    create template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+const api = {
+  key: "d0abb125c9a030c9aac4a7db57fec36c",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
-export default App;
+const App = () => {
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+          />
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
